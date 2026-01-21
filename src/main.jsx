366b5d7378ab17175ee56b22330b1d29e4f06014
Abdur Rehman <abdurrehmananqayyum@gmail.com>
2026-01-22 03:45:10 +0500

    Logo changed

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,8 @@ import { createRoot } from "react-dom/client";
 import logo from "./assets/images/react-img.png";
 
 
- 
-
 const root = createRoot(document.getElementById("root"))
 
-root.render(
-
-
+root.render( 
     <img src={logo} alt="Logo" />
-)
+) 
