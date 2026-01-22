572788a771533be40f4a16fb92fd0c47bb4a97ee
Abdur Rehman <abdurrehmananqayyum@gmail.com>
2026-01-22 18:50:46 +0500

    Basic info Added

--- src/main.jsx
+++ src/main.jsx
@@ -5,5 +5,15 @@ import logo from "./assets/images/react-img.png";
 const root = createRoot(document.getElementById("root"))
 
 root.render( 
-    <img src={logo} width="40px" alt="Logo" />
+    <main>
+    <img src={logo} width="90px" alt="Logo" />
+    <h1>Fun Facts About React!</h1>
+    <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walket</li>
+        <li>Has well over 200k Stars on Github</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+    </main>
 )
